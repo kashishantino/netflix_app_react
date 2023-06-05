51244e5dcd911271f23da29258ee5df80dd08093
kashishantino <kashish.y@antino.io>
2023-06-05 16:53:54 +0530

    commiting whole netflix app project to github

---
+++ src/Data.js
@@ -0,0 +1,51 @@
+import img1 from './image/money.jfif'
+import img2 from './image/stranger.jfif'
+import img3 from './image/dark.jfif'
+import img4 from './image/guilty.jfif'
+import img5 from './image/daredevil.jfif'
+import img6 from './image/mismatch.jfif'
+const Data =[
+    {
+        sname:"MONEY HEIST",
+        imgsrc:img1,
+        title:"neetflox",
+        link:"https://google.com"
+    },
+    {
+        sname:"STRANGER THINGS",
+        imgsrc:img2,
+        title:"neetflox",
+        link:"https://google.com"
+    },
+    {
+        sname:"DARK",
+        imgsrc:img3,
+        title:"neetflox",
+        link:"https://google.com"
+    },
+    {
+        sname:"GUILTY",
+        imgsrc:img4,
+        title:"neetflox",
+        link:"https://google.com"
+    },
+    {
+        sname:"DAREDEVIL",
+        imgsrc:img5,
+        title:"neetflox",
+        link:"https://google.com"
+    },
+    {
+        sname:"mismatch",
+        imgsrc:img6,
+        title:"neetflox",
+        link:"https://google.com"
+    },
+    {
+        sname:"mismatch",
+        imgsrc:img6,
+        title:"neetflox",
+        link:"https://google.com"
+    }
+]
+export default Data;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDOM  from 'react-dom';
+import Card from "./Cards";
+import  Data from './Data';
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<>
+<h1 className='heading_style'>List of top Netflix Series in 2020</h1>
+  {
+    Data.map(val=>{
+      console.log(val);
+      return<>
+      <Card
+    title={val.title}
+    sname={val.sname}
+    imgsrc={val.imgsrc}
+  />
+      </>
+    })
+  }
+   
+  </>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
